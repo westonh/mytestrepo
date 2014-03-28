72d3b2c8c0e8713a1b8fd44ab8d61d81b22341bb
Weston <whutch@gmail.com>
2014-03-28 10:29:29 -0700

    updated hello world text

--- helloworld.py
+++ helloworld.py
@@ -5,7 +5,7 @@ class MainPage(webapp2.RequestHandler):
 
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, webapp2 World22!')
+        self.response.write('Hello, webapp2 World!')
 
 
 application = webapp2.WSGIApplication([
